5c6660a6ac173139265f828873948e8cea9a5ffd
Darya Zata <darya.zata@outlook.com>
2022-08-11 21:41:36 +0200

    Updates

---
+++ precache-manifest.365cdf2acd1fe4deffad10f833fb544a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808f0c821b988311f063d3f2644b261c",
+    "url": "/Netflix_Clone/index.html"
+  },
+  {
+    "revision": "635f5e4166ed0959a7ef",
+    "url": "/Netflix_Clone/static/css/main.4f9655fb.chunk.css"
+  },
+  {
+    "revision": "3632fc0db476bd6c1570",
+    "url": "/Netflix_Clone/static/js/2.b98776ab.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix_Clone/static/js/2.b98776ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635f5e4166ed0959a7ef",
+    "url": "/Netflix_Clone/static/js/main.af8bdb4e.chunk.js"
+  },
+  {
+    "revision": "24b13dd79d4c36122315",
+    "url": "/Netflix_Clone/static/js/runtime-main.0c1c498d.js"
+  },
+  {
+    "revision": "e25ef8e4b28eccc20a88d82a18ee9c2d",
+    "url": "/Netflix_Clone/static/media/netflix-user.e25ef8e4.jpg"
+  }
+]);
